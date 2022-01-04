8cc5ba7c78545d950fe10caeb781c867e9cc676b
Shelby B <82240184+shelby-bennett@users.noreply.github.com>
2022-01-04 16:36:40 -0500

    Update working_loadat.R
    updated current working version

--- working_loadat.R
+++ working_loadat.R
@@ -7,19 +7,19 @@ library(wesanderson)
 library(lubridate)
 library(sqldf)
 library(stringr)
-library(outbreakinfo)
+#library(outbreakinfo)
 ###read in raw data
 
 raw_d_data = read_csv("updatedv1216.csv", col_types = cols_only(taxon = col_guess(), lineage = col_guess()))
 
-raw_mstrlist = read_csv("mstrlst_1123.csv", col_types = cols_only(seq_id = col_guess(),
+raw_mstrlist = read_csv("mstr_lst122821.csv", col_types = cols_only(seq_id = col_guess(),
                                                                   r_num = col_guess(),
                                                                   collection_date = col_date(format = "%m/%d/%Y"),
                                                                   `City/County` = col_guess(), `Health District` = col_guess(),
                                                                   `Health Region` = col_guess()))
 
 raw_g_data =read_csv("gisaid_122921.csv", col_types = cols_only(`Virus name` = col_guess(), 
-                                                             `Collection date` = col_date(format = "%Y-%m-%d"), 
+                                                             `Collection date` = col_date(format = "%m/%d/%Y"), 
                                                              Lineage = col_guess()))
 #####################################################################################################################
 ### DATA CLEAN UP ###
@@ -31,21 +31,22 @@ combd_dat = full_join(raw_mstrlist, form_d_data, by = "seq_id")
 
 #seperating DCLS GISAID data from all other submitters
 form_g_data = raw_g_data %>% 
-  select(virus_name = `Virus name` ,collection_date = `Collection date`, lineage_PANGO_lineage = "Lineage") %>% 
-  filter(str_detect(virus_name, "DCLS"))
+  select(seq_id = `Virus name` ,collection_date = `Collection date`, lineage_PANGO_lineage = "Lineage") %>% 
+  filter(str_detect(seq_id, "DCLS"))
 
-  
+#all gisaid data  
 all_gdata = raw_g_data %>% 
-  select(virus_name = `Virus name` ,collection_date = `Collection date`, lineage_PANGO_lineage = "Lineage")
+  select(seq_id = `Virus name` ,collection_date = `Collection date`, lineage_PANGO_lineage = "Lineage")
 
 
 ####################################################################################################################
 ## Establish some static lineage information ##
 
-#grab all of the possible AY bastards for each tible
+#grab all of the possible AY bastards for each tibble
 
 all_gay = all_gdata[grepl("AY", all_gdata$lineage_PANGO_lineage),]
-all_day = form_g_data[grepl("AY", form_g_data$lineage_PANGO_lineage),]
+all_gay = all_gay[-2]
+all_day = form_d_data[grepl("AY", form_d_data$lineage_PANGO_lineage),]
 all_ay = rbind(all_gay,all_day)
 
 list_ay = unique(all_ay$lineage_PANGO_lineage)
@@ -68,6 +69,8 @@ who_variants = list(
   'Zeta' = c("P.2"),
   'Omicron' = c("B.1.1.529","BA.1", "BA.2")
 )
+whovar_df = map_df(who_variants, ~as.data.frame(.x), .id="variant")
+names(whovar_df)[2] = "lineage_PANGO_lineage"
 
 WHO_VOC = c(
   'Delta',
@@ -94,12 +97,34 @@ VBM_list = c("B.1.617.3", who_variants$Alpha, who_variants$Beta, who_variants$Ga
                who_variants$Mu, who_variants$Zeta)
 
 ########################################################################################################################
-##final clean u
-
+##final clean up
+#only DCLS data
 final_d_data_var = combd_dat %>% 
   drop_na(r_num,lineage_PANGO_lineage,collection_date) %>% 
   distinct(seq_id, .keep_all = TRUE) %>% 
   group_by(lineage_PANGO_lineage) %>%
   mutate(lineage_PANGO_lineage = ifelse(lineage_PANGO_lineage %in% unlist(who_variants, use.names = F), 
-                                        lineage_PANGO_lineage,"Other")) %>% 
-  mutate(who_var = ifelse())
+                                        lineage_PANGO_lineage,"Other"))
+final_d_data_var2 = left_join(final_d_data_var, whovar_df, by = "lineage_PANGO_lineage")
+final_d_data_var2$variant = final_d_data_var2$variant %>% 
+  replace_na("Other")
+#Only DCLS data in GISAID
+final_dg_data = form_g_data %>% 
+  distinct(seq_id, .keep_all = TRUE) %>% 
+  group_by(lineage_PANGO_lineage) %>%
+  mutate(lineage_PANGO_lineage = ifelse(lineage_PANGO_lineage %in% unlist(who_variants, use.names = F), 
+                                        lineage_PANGO_lineage,"Other"))
+final_dg_datavar2 = left_join(final_dg_data,whovar_df, by = "lineage_PANGO_lineage")
+final_dg_datavar2$variant = final_dg_datavar2$variant %>% 
+  replace_na("Other")  
+
+#All GISAID data
+final_g_data_var = all_gdata %>% 
+  distinct(seq_id, .keep_all = TRUE) %>% 
+  group_by(lineage_PANGO_lineage) %>%
+  mutate(lineage_PANGO_lineage = ifelse(lineage_PANGO_lineage %in% unlist(who_variants, use.names = F), 
+                                        lineage_PANGO_lineage,"Other"))
+final_g_data_var2 = left_join(final_g_data_var, whovar_df, by = "lineage_PANGO_lineage")
+final_g_data_var2$variant = final_g_data_var2$variant %>% 
+  replace_na("Other")
+  
